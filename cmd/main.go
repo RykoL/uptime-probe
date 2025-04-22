0a744efb686a0de4ca2ede5237721063e4852c20
Rico Lang <rico.lang@codecentric.de>
2025-04-22 10:28:24 +0200

    refactor: use view model to display monitors

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
-	"github.com/RykoL/uptime-probe/internal/monitor"
+	"github.com/RykoL/uptime-probe/web/model"
 	"github.com/RykoL/uptime-probe/web/static"
 	"github.com/a-h/templ"
 	"net/http"
 )
 
 func main() {
-	m := monitor.NewMonitor("My Monitor")
-	http.Handle("/", templ.Handler(static.Index([]monitor.Monitor{m})))
+	monitors := []model.Monitor{
+		{Name: "GCP", Status: "Up"},
+		{Name: "AWS", Status: "Up"},
+		{Name: "Azure", Status: "Unknown"},
+		{Name: "StackIt", Status: "Pending"},
+		{Name: "IONOS", Status: "Down"},
+	}
+	http.Handle("/", templ.Handler(static.Index(monitors)))
 
 	http.ListenAndServe(":8080", nil)
 }
