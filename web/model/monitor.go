c5f9d3377cadf05c7dff6acc58d8d8871b84dd4e
Rico Lang <rico.lang@codecentric.de>
2025-04-22 10:31:56 +0200

    fix: tests breaking because of change to view model

---
+++ web/model/monitor.go
@@ -0,0 +1,6 @@
+package model
+
+type Monitor struct {
+	Name   string
+	Status string
+}
--- web/static/monitor_test.go
+++ web/static/monitor_test.go
@@ -1,34 +1,33 @@
 package static
 
 import (
-	"github.com/RykoL/uptime-probe/internal/monitor"
 	"github.com/RykoL/uptime-probe/internal/testutils"
+	"github.com/RykoL/uptime-probe/web/model"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestRendersMonitorName(t *testing.T) {
-	m := monitor.NewMonitor("SomeMonitor")
+	m := model.Monitor{Name: "SomeMonitor"}
 	comp := testutils.RenderComponent(Monitor(m))
 
 	assert.Equal(t, "SomeMonitor", comp.Find(`h2`).Text())
 }
 
 type statusCase struct {
-	Status monitor.Status
-	Text   string
+	Text string
 }
 
 func TestRendersStatus(t *testing.T) {
 	cases := []statusCase{
-		{monitor.StatusDown, "Down"},
-		{monitor.StatusUp, "Up"},
-		{monitor.StatusUnknown, "Unknown"},
-		{monitor.StatusPending, "Pending"},
+		{"Down"},
+		{"Up"},
+		{"Unknown"},
+		{"Pending"},
 	}
 
 	for _, testCase := range cases {
-		comp := testutils.RenderComponent(MonitorStatus(testCase.Status))
+		comp := testutils.RenderComponent(MonitorStatus(testCase.Text))
 
 		assert.Equal(t, testCase.Text, comp.Find(`span`).Text())
 
